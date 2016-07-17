bccfc5906cb0d989cd49c8a67b000acade25a061
Simon <s.w@sconts.com>
2016-07-17 19:05:21 +0800

    add loading page

--- action/forms.py
+++ action/forms.py
@@ -9,7 +9,7 @@ class EtcdClusterForm(forms.ModelForm):
         
 class KeyForm(forms.Form):
     key_path = forms.CharField(label='Key path')
-    value = forms.CharField(label='Value to set', required=False)
+    value = forms.CharField(label='Value to set', widget=forms.Textarea(attrs={'rows':10, 'cols':6}), required=False)
     ttl =  forms.IntegerField(min_value=0)
     is_dir = forms.BooleanField(required=False ,label='Is a directory')
     append = forms.BooleanField(required=False)
